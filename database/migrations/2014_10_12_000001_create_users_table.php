0d75ac836a78204e7b7be0ee9d6203bbb5676770
Anastasia <anastasiakosovets@gmail.com>
2023-07-10 19:50:04 +0200

    feat: start with php laravel Eloquent ORM project

---
+++ database/migrations/2014_10_12_000001_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('document');
+            $table->string('dateOfBirth');
+            $table->string('address');
+            $table->string('telefonNumber');
+            $table->string('collegialNumber');
+            $table->unsignedBigInteger('role_id');
+            
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
