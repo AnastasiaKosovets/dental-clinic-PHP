0f826d3e4a129ad3187f9e29c3c278c777044660
Anastasia <anastasiakosovets@gmail.com>
2023-07-11 23:26:28 +0200

    feat: getAllAppointments working with details of appointment using query and foreach methods

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -32,7 +32,7 @@ class AppointmentController extends Controller
             // function ($query) es una función anónima en PHP. restricción/persionalización
             // de las relaciones. Puedo definir instrucciones adicionales utilizando select
             // en conjunto con $query
-            
+
             $appointments = Appointment::with([
                 'patient' => function ($query) {
                     $query->select('id', 'firstName', 'lastName');
@@ -54,22 +54,48 @@ class AppointmentController extends Controller
             // se utiliza método 'getAttribute()' que nos permite acceder a los valores
             //  de los atributos específicos de las relaciones
             // accedemos a la primera cita médica en la respuesta JSON utilizando el índice
-            $appointment = $appointments[0];
-            $patientFirstName = $appointment->patient->getAttribute('firstName');
-            $patientLastName = $appointment->patient->getAttribute('lastName');
-            $treatmentName = $appointment->treatment->getAttribute('treatmentName');
-            $treatmentDescription = $appointment->treatment->getAttribute('description');
-            $doctorFirstName = $appointment->doctor->getAttribute('firstName');
-            $doctorLastName = $appointment->doctor->getAttribute('lastName');
+
+            // $appointment = $appointments[0];
+            // $patientFirstName = $appointment->patient->getAttribute('firstName');
+            // $patientLastName = $appointment->patient->getAttribute('lastName');
+            // $treatmentName = $appointment->treatment->getAttribute('treatmentName');
+            // $treatmentDescription = $appointment->treatment->getAttribute('description');
+            // $doctorFirstName = $appointment->doctor->getAttribute('firstName');
+            // $doctorLastName = $appointment->doctor->getAttribute('lastName');
+
+            $appointmentData = [];
+            foreach ($appointments as $appointment) {
+                $patientFirstName = $appointment->patient->getAttribute('firstName');
+                $patientLastName = $appointment->patient->getAttribute('lastName');
+                $treatmentName = $appointment->treatment->getAttribute('treatmentName');
+                $treatmentDescription = $appointment->treatment->getAttribute('description');
+                $doctorFirstName = $appointment->doctor->getAttribute('firstName');
+                $doctorLastName = $appointment->doctor->getAttribute('lastName');
+
+                $appointmentData[] = [
+                    'patientFirstName' => $patientFirstName,
+                    'patientLastName' => $patientLastName,
+                    'treatmentName' => $treatmentName,
+                    'treatmentDescription' => $treatmentDescription,
+                    'doctorFirstName' => $doctorFirstName,
+                    'doctorLastName' => $doctorLastName,
+                ];
+            }
+
+            // return response()->json([
+            //     'message' => 'Appointment retrieved',
+            //     'patientFirstName' => $patientFirstName,
+            //     'patientLastName' => $patientLastName,
+            //     'treatmentName' => $treatmentName,
+            //     'treatmentDescription' => $treatmentDescription,
+            //     'doctorFirstName' => $doctorFirstName,
+            //     'doctorLastName' => $doctorLastName,
+            //     'success' => true
+            // ], Response::HTTP_OK);
 
             return response()->json([
-                'message' => 'Appointment retrieved',
-                'patientFirstName' => $patientFirstName,
-                'patientLastName' => $patientLastName,
-                'treatmentName' => $treatmentName,
-                'treatmentDescription' => $treatmentDescription,
-                'doctorFirstName' => $doctorFirstName,
-                'doctorLastName' => $doctorLastName,
+                'message' => 'Appointments retrieved',
+                'data' => $appointmentData,
                 'success' => true
             ], Response::HTTP_OK);
         } catch (\Throwable $th) {
